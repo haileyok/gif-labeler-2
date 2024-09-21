f1beba5f1b509f4a289496dfb63016a322d48404
notjuliet <notjuliet@riseup.net>
2024-09-21 17:48:17 +0200

    video labeler

--- src/label.ts
+++ src/label.ts
@@ -1,12 +1,4 @@
-import { ComAtprotoLabelDefs } from "@atcute/client/lexicons";
-import {
-  DID,
-  PORT,
-  LABEL_LIMIT,
-  POSTS,
-  SIGNING_KEY,
-  DELETE,
-} from "./constants.js";
+import { DID, PORT, SIGNING_KEY } from "./constants.js";
 import { LabelerServer } from "@skyware/labeler";
 
 const server = new LabelerServer({ did: DID, signingKey: SIGNING_KEY });
@@ -16,31 +8,10 @@ server.start(PORT, (error, address) => {
   else console.log(`Labeler server listening on ${address}`);
 });
 
-export const label = async (did: string, rkey: string) => {
-  const query = server.db
-    .prepare<
-      unknown[],
-      ComAtprotoLabelDefs.Label
-    >(`SELECT * FROM labels WHERE uri = ?`)
-    .all(did);
-
-  const labels = query.reduce((set, label) => {
-    if (!label.neg) set.add(label.val);
-    else set.delete(label.val);
-    return set;
-  }, new Set<string>());
-
+export const label_video = async (uri: string) => {
   const time = new Date().toISOString();
-
-  if (rkey.includes(DELETE)) {
-    await server
-      .createLabels({ uri: did }, { negate: [...labels] })
-      .catch((err) => console.log(err))
-      .then(() => console.log(`${time}: Deleted labels for ${did}`));
-  } else if (labels.size < LABEL_LIMIT && POSTS[rkey]) {
-    await server
-      .createLabel({ uri: did, val: POSTS[rkey] })
-      .catch((err) => console.log(err))
-      .then(() => console.log(`${time}: Labeled ${did} with ${POSTS[rkey]}`));
-  }
+  await server
+    .createLabel({ uri: uri, val: "video" })
+    .catch((err) => console.log(err))
+    .then(() => console.log(`${time}: Labeled video: ${uri}`));
 };
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
-import { label } from "./label.js";
-import { DID } from "./constants.js";
+import { label_video } from "./label.js";
 import fs from "node:fs";
 import { Jetstream } from "@skyware/jetstream";
-import { AppBskyFeedLike } from "@atcute/client/lexicons";
 
 let intervalID: NodeJS.Timeout;
 const cursorFile = fs.readFileSync("cursor.txt", "utf8");
 if (cursorFile) console.log(`Initiate firehose at cursor ${cursorFile}`);
 
 const jetstream = new Jetstream({
-  wantedCollections: ["app.bsky.feed.like"],
+  wantedCollections: ["app.bsky.feed.post"],
   cursor: cursorFile ?? 0,
 });
 
@@ -28,10 +26,9 @@ jetstream.on("error", (err) => console.error(err));
 
 jetstream.on("close", () => clearInterval(intervalID));
 
-jetstream.onCreate("app.bsky.feed.like", (event) => {
-  const record = event.commit.record as AppBskyFeedLike.Record;
-  if (record.subject?.uri?.includes(`${DID}/app.bsky.feed.post`))
-    label(event.did, record.subject.uri.split("/").pop()!);
+jetstream.onCreate("app.bsky.feed.post", (event) => {
+  if (event.commit.record.embed?.$type === "app.bsky.embed.video")
+    label_video(`at://${event.did}/app.bsky.feed.post/${event.commit.rkey}`);
 });
 
 jetstream.start();

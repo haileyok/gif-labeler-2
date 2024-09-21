1cf4e976ebc0a6c355c9e74fc0870b06372d7b67
Hailey <me@haileyok.com>
2024-09-21 11:16:33 -0700

    remove cursor thanks julie

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,13 @@
 import { labelGif } from './label.js'
-import fs from "node:fs";
 import { Jetstream } from "@skyware/jetstream";
 import {JETSTREAM_ENDPOINT} from './constants.js'
-
-let intervalID: NodeJS.Timeout;
-const cursorFile = fs.readFileSync("cursor.txt", "utf8");
-if (cursorFile) console.log(`Initiate firehose at cursor ${cursorFile}`);
 
 const jetstream = new Jetstream({
   endpoint: JETSTREAM_ENDPOINT,
   wantedCollections: ["app.bsky.feed.post"],
-  cursor: cursorFile ?? 0,
-});
-
-jetstream.on("open", () => {
-  intervalID = setInterval(() => {
-    if (jetstream.cursor) {
-      console.log(`${new Date().toISOString()}: ${jetstream.cursor}`);
-      fs.writeFile("cursor.txt", jetstream.cursor.toString(), (err) => {
-        if (err) console.log(err);
-      });
-    }
-  }, 60000);
 });
 
 jetstream.on("error", (err) => console.error(err));
-
-jetstream.on("close", () => clearInterval(intervalID));
 
 jetstream.onCreate("app.bsky.feed.post", (event) => {
   const embed = event.commit.record.embed;
